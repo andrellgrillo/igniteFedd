8cf129aa0c85109d4a05c330b3f6bee1559a36b6
Andre Grillo <andrell.grillo@gmail.com>
2022-06-30 16:29:04 -0300

    Componente Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,30 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/andrellgrillo.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>André Grillo</strong>
+              <time title="30 de Junho às 14:50h" dateTime='2022-06-30 14:50:00'>Publicado há 1h</time>
+            </div>
+            <button title='Deletar comentário'>
+              <Trash />
+            </button>
+          </header>
+          <p>Muito bom. Parabéns</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
