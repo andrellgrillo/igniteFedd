33b23deb2eb88416a1eb0cd92856e2b7e32885dc
Andre Grillo <andrell.grillo@gmail.com>
2022-07-02 09:15:44 -0300

    Componente Avatar - Introdução a responsividade

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -9,7 +10,7 @@ export function Sidebar() {
         className={styles.cover} 
         src="https://images.unsplash.com/photo-1608153513681-5eed4ecddb61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/26194953?v=4" alt="" />
+        <Avatar src="https://github.com/diego3g.png" />
         <strong>Andre Grillo</strong>
         <span>Wev Developer</span>
       </div>
